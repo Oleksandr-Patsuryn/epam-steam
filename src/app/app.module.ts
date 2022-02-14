b3a46a456a448007ee6fde3df6204a2b952c0492
aleksbuk <booker.ua@gmail.com>
2022-02-14 22:25:23 +0200

    init: start friend section

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { FriendComponent } from './components/friend/friend.component';
+import { FriendSerchComponent } from './components/friend-serch/friend-serch.component';
+import { FriendListComponent } from './components/friend-list/friend-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FriendComponent,
+    FriendSerchComponent,
+    FriendListComponent
   ],
   imports: [
     BrowserModule
