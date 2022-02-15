c5409cb6bdf5c3c0c4d2604ed0f40dfb80d3ff49
exiio <oleksihshnko@gmail.com>
2022-02-15 17:45:17 +0200

    Commit 2. Profile page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppComponent } from './app.component';
 import { LoginComponent } from './components/login/login.component';
 import { HeaderComponent } from './components/header/header.component';
 import { ProfileComponent } from './components/profile/profile.component';
+import { ProfileWrapperComponent } from './components/profile-wrapper/profile-wrapper.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
     HeaderComponent,
-    ProfileComponent
+    ProfileComponent,
+    ProfileWrapperComponent
   ],
   imports: [
     BrowserModule
