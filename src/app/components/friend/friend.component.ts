ee721d134e7bdb13dda297868e881bdd8ff19773
Patsu-rar <opatsurin@gmail.com>
2022-03-17 12:24:18 +0200

    Some fixes.

--- src/app/components/friend/friend.component.ts
+++ src/app/components/friend/friend.component.ts
@@ -1,10 +1,22 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {User, AddUserService} from "../../service/add-user-service";
 
 @Component({
   selector: 'app-friend',
   templateUrl: './friend.component.html',
   styleUrls: ['./friend.component.css']
 })
-export class FriendComponent {
-  constructor() { }
+export class FriendComponent implements OnInit {
+  users: User[] = [];
+  searchUser: string = '';
+  username: string = '';
+
+  constructor(private userService: AddUserService) {
+  }
+
+  ngOnInit(): void {
+    this.userService.getData().subscribe((users) => {
+      this.users = users;
+    })
+  }
 }
